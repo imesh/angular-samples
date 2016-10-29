ccdcd8de3059dbf1e0db45307798e7ca55be2cf2
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2016-10-29 23:11:29 +0530

    Initial commit

---
+++ quick-start/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular App</h1><p>Written in Angular 2!</p>'
+})
+export class AppComponent { }
